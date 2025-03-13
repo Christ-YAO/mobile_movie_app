ac4806f2f3e5862f03e1fbce65414838e2e4f400
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2025-03-13 16:23:09 +0000

    Setup Appwrite

---
+++ services/appwrite.ts
@@ -0,0 +1,56 @@
+import {
+  Client,
+  Databases,
+  ID,
+  Permission,
+  Query,
+} from "react-native-appwrite";
+
+const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
+const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
+
+const client = new Client()
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!);
+
+const database = new Databases(client);
+
+export const updateSearchCount = async (query: string, movie: Movie) => {
+  console.log("DATABASE_ID:", DATABASE_ID);
+  console.log("COLLECTION_ID:", COLLECTION_ID);
+  console.log("PROJECT_ID:", process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID);
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.equal("searchTerm", query),
+    ]);
+
+    if (result.documents.length > 0) {
+      const existingMovie = result.documents[0];
+      await database.updateDocument(
+        DATABASE_ID,
+        COLLECTION_ID,
+        existingMovie.$id,
+        {
+          count: existingMovie.count + 1,
+        }
+      );
+    } else {
+      await database.createDocument(
+        DATABASE_ID,
+        COLLECTION_ID,
+        ID.unique(),
+        {
+          searchTerm: query,
+          movie_id: movie.id,
+          title: movie.title,
+          count: 1,
+          poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+        },
+        [Permission.read("role:all"), Permission.write("role:all")]
+      );
+    }
+  } catch (error) {
+    console.error("Error updating search count:", error);
+    throw error;
+  }
+};
