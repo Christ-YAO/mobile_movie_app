e5b2b1c7b8283a57a16a689a7a1fdcee4465f373
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2025-03-22 23:38:11 +0000

    Reorganized my code

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -1,17 +1,11 @@
-import {
-  Client,
-  Databases,
-  ID,
-  Permission,
-  Query,
-} from "react-native-appwrite";
+import { Client, Databases, ID, Query } from "react-native-appwrite";
 
 const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
 const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
 
 const client = new Client()
   .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject("67cc8a54000d5a602359");
+  .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!);
 
 const database = new Databases(client);
 
@@ -21,10 +15,8 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
       Query.equal("searchTerm", query),
     ]);
 
-    const documents = [...result.documents];
-
-    if (documents.length > 0) {
-      const existingMovie = documents[0];
+    if (result.documents.length > 0) {
+      const existingMovie = result.documents[0];
       await database.updateDocument(
         DATABASE_ID,
         COLLECTION_ID,
@@ -34,25 +26,17 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
         }
       );
     } else {
-      await database.createDocument(
-        DATABASE_ID,
-        COLLECTION_ID,
-        ID.unique(),
-        {
-          searchTerm: query,
-          movie_id: movie.id,
-          title: movie.title,
-          count: 1,
-          poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
-        },
-        [Permission.read("role:all"), Permission.write("role:all")]
-      );
+      await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+        searchTerm: query,
+        movie_id: movie.id,
+        title: movie.title,
+        count: 1,
+        poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      });
     }
-  } catch (error: any) {
-    console.error(
-      "Error updating search count:",
-      error.response?.message || error.message || error
-    );
+  } catch (error) {
+    console.error("Error updating search count:", error);
+    throw error;
   }
 };
 
